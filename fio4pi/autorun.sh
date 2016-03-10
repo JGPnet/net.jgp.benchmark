ed83e6d2a4da3db25de3d6170bb3782c7797f023
jgp <jgp@jgp.net>
2016-03-10 18:09:46 +0000

    Update of tests, refactor, added a script

---
+++ fio4pi/autorun.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Drive id ... " $1
+echo "Platform ... " $2
+echo "Version .... " `uname -r`
+if [ -z $1 ]
+then
+  echo "Must provide drive id"
+  exit 1
+fi
+if [ -z $2 ]
+then
+  echo "Must provide platform id (rpi2, rpi3)"
+  exit 1
+fi
+echo "1 reader..."
+/usr/local/bin/fio random-1reader-test.fio > results/$1-1r-$2-raspbian_v`uname -r`.log
+echo "1 writer..."
+/usr/local/bin/fio random-1writer-test.fio > results/$1-1w-$2-raspbian_v`uname -r`.log
+echo "2 readers..."
+/usr/local/bin/fio random-2readers-test.fio > results/$1-2r-$2-raspbian_v`uname -r`.log
+echo "2 writers..."
+/usr/local/bin/fio random-2writers-test.fio > results/$1-2w-$2-raspbian_v`uname -r`.log
+echo "4 readers..."
+/usr/local/bin/fio random-4readers-test.fio > results/$1-4r-$2-raspbian_v`uname -r`.log
+echo "4 writers..."
+/usr/local/bin/fio random-4writers-test.fio > results/$1-4w-$2-raspbian_v`uname -r`.log
+echo "6 readers..."
+/usr/local/bin/fio random-6readers-test.fio > results/$1-6r-$2-raspbian_v`uname -r`.log
+echo "8 readers..."
+/usr/local/bin/fio random-8readers-test.fio > results/$1-8r-$2-raspbian_v`uname -r`.log

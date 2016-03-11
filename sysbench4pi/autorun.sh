6ace56c5a75c7112e76fe622a566b54154824251
jgp <jgp@jgp.net>
2016-03-11 17:33:58 +0000

    more threads

--- sysbench4pi/autorun.sh
+++ sysbench4pi/autorun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for each in 1 2 4 8 16 32 64; do sysbench --test=cpu --cpu-max-prime=20000 --num-threads=$each run; done
+for each in 1 2 3 4 5 6 8 16 32 64; do sysbench --test=cpu --cpu-max-prime=60000 --num-threads=$each run; done

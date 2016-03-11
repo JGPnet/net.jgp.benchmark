23b54dc3a52222363e6878a656356fa23badc208
jgp <jgp@jgp.net>
2016-03-11 20:42:53 +0000

    Friendlier shell

--- sysbench4pi/autorun.sh
+++ sysbench4pi/autorun.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-for each in 1 2 3 4 5 6 8 16 32 64; do sysbench --test=cpu --cpu-max-prime=60000 --num-threads=$each run; done
+
+# Loop
+for each in 1 2 3 4 5 6 8 16 32 64
+do 
+  echo Running $each Threads...
+  sysbench --test=cpu --cpu-max-prime=60000 --num-threads=$each run
+done

ffc0c314a48c718806c467fd7bf701c18f678b3a
jgperrin <jgperrin@users.noreply.github.com>
2016-03-11 19:29:17 -0500

    Update autorun.sh

--- sysbench4pi/autorun.sh
+++ sysbench4pi/autorun.sh
@@ -3,6 +3,7 @@
 # Loop
 for each in 1 2 3 4 5 6 8 16 32 64
 do 
-  echo Running $each Threads...
+  echo "--------------------------------------------------------------------------------"
+  echo Running $each thread\(s\)...
   sysbench --test=cpu --cpu-max-prime=60000 --num-threads=$each run
 done

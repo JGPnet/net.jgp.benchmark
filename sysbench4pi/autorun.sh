ecd0144c34437ff992987cfea49cf0245df530e2
jgperrin <jgperrin@users.noreply.github.com>
2016-03-11 20:51:13 -0500

    Update autorun.sh

--- sysbench4pi/autorun.sh
+++ sysbench4pi/autorun.sh
@@ -5,6 +5,8 @@ version=`uname -r`
 echo "Platform ...... " $platform
 echo "OS Version .... " $version
 
+./sensetemp.sh > results/cputemp-$platform-$version.log &
+
 rm results/cpu-$platform-$version.log
 
 # Loop
@@ -15,11 +17,16 @@ do
   for rec in {1..3}
   do
     echo "Test #$rec (threads: $each)"
+    echo "--------------------------------------------------------------------------------" >> results/cputemp-$platform-$version.log
+    echo "Test #$rec (threads: $each)" >> results/cputemp-$platform-$version.log
     sysbench --test=cpu --cpu-max-prime=200 --num-threads=$thread run > results/cpu-$platform-$version-$thread-$rec.log
     cat results/cpu-$platform-$version-$thread-$rec.log
     echo "--------------------------------------------------------------------------------" >> results/cpu-$platform-$version.log
     cat results/cpu-$platform-$version-$thread-$rec.log >> results/cpu-$platform-$version.log
     rm results/cpu-$platform-$version-$thread-$rec.log
+    echo "--------------------------------------------------------------------------------" >> results/cputemp-$platform-$version.log
     sleep 20s
   done
 done
+
+echo [done]

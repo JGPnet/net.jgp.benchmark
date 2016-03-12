a4293c0d205c84becae8865baf8c6e723bbfa935
jgperrin <jgperrin@users.noreply.github.com>
2016-03-11 20:21:03 -0500

    Update autorun.sh

--- sysbench4pi/autorun.sh
+++ sysbench4pi/autorun.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+$platform = `cat /proc/cmdline | awk -v RS=" " -F= '/boardrev/ { print $2 }'`
+$version = `uname -r`
+echo "Platform ...... " $platform
+echo "OS Version .... " $version
+
+rm results/cpu-$platform-$version.log
+
 # Loop
-for each in 1 2 3 4 5 6 8 16 32 64
+for thread in 1 2 3 4 5 6 8 16 32 64
 do 
   echo "--------------------------------------------------------------------------------"
-  echo Running $each thread\(s\)...
-  for rec in [1..3]
+  echo Running $thread thread\(s\)...
+  for rec in {1..3}
   do
     echo "Test #$rec (threads: $each)"
-    sysbench --test=cpu --cpu-max-prime=200 --num-threads=$each run
+    sysbench --test=cpu --cpu-max-prime=200 --num-threads=$thread run > results/cpu-$platform-$version-$thread-$rec.log
+    cat results/cpu-$platform-$version-$thread-$rec.log
+    cat results/cpu-$platform-$version-$thread-$rec.log > results/cpu-$platform-$version.log
+    rm results/cpu-$platform-$version-$thread-$rec.log
     sleep 20s
   done
 done

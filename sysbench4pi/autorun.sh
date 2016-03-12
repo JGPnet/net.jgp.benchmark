68f621163b4706b6cefe54d54a82c8437008a511
jgperrin <jgperrin@users.noreply.github.com>
2016-03-11 20:00:26 -0500

    Update autorun.sh

--- sysbench4pi/autorun.sh
+++ sysbench4pi/autorun.sh
@@ -5,5 +5,10 @@ for each in 1 2 3 4 5 6 8 16 32 64
 do 
   echo "--------------------------------------------------------------------------------"
   echo Running $each thread\(s\)...
-  sysbench --test=cpu --cpu-max-prime=60000 --num-threads=$each run
+  for rec in [1..3]
+  do
+    echo "Test #$rec (threads: $each)"
+    sysbench --test=cpu --cpu-max-prime=200 --num-threads=$each run
+    sleep 20s
+  done
 done
